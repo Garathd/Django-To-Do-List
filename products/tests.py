826b3ca28552b74e04a9f38e1d44b9db1c253de6
Garath Davis <garath.davis.gti@gmail.com>
2019-06-16 15:00:43 +0000

    Added Product Model and View Unit Tests

--- products/tests.py
+++ products/tests.py
@@ -1,13 +1,37 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from .models import Product
+from accounts.client import Login
 
-# Create your tests here.
-class ProductTests(TestCase):
-    """
-    Here we'll define the tests that we'll run against our
-    Product model
-    """
 
-    def test_str(self):
-        test_name = Product(name='A product')
-        self.assertEqual(str(test_name), 'A product')
+"""
+Test the Product View
+"""
+class ProductViewTest(TestCase):
+    
+    # This runs a testing client
+    c = Client()
+
+    def test_all_products(self):
+        Login.setUp(self)
+        page = self.c.get("/products/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "products.html")
+    
+    
+"""
+Test the Product Model
+"""
+class ProductModelTests(TestCase):
+
+    def test_product_model(self):
+        
+        product = Product(
+            name = 'Product',
+            description = 'Example Description',
+            price = '1.99'    
+        )
+        product.save()
+        
+        self.assertEqual(product.name, 'Product')
+        self.assertEqual(product.description, 'Example Description')
+        self.assertEqual(product.price, '1.99')
