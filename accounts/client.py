8b7931fb14fdd95da940caf067f246a6803c60b8
Garath Davis <garath.davis.gti@gmail.com>
2019-06-16 12:03:48 +0000

    Added Cart View Test Unit test

--- accounts/client.py
+++ accounts/client.py
@@ -1,8 +1,12 @@
+from django.test import Client
 from .forms import UserRegistrationForm
 
 class Login:
+    
+    c = Client()
 
     def setUp(self):
+
         user = UserRegistrationForm({"username":"test", "email":"test@test.com", "password1":"access", "password2":"access"})
         this_user = user.save()
         logged_in = self.c.login(username='test', password='access')
--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from accounts.client import Login
 
 # Create your tests here.
+class CartViewTest(TestCase):
+    
+    # This runs a testing client
+    c = Client()
+    
+    def test_cart_view(self):
+        Login.setUp(self)
+        page = self.c.get("/cart/")
+        # Checking to see if page redirects because cart is empty
+        self.assertEqual(page.status_code, 302)
+
+
+ 
