ece444e83cb975b8f787c2cd4ab3be5642740c5d
Garath Davis <garath.davis.gti@gmail.com>
2019-06-16 13:41:09 +0000

    Added Dashboard View Test

--- dashboard/tests.py
+++ dashboard/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from accounts.client import Login
 
-# Create your tests here.
+
+"""
+Checks the if logged in user can access the dashboard page
+"""
+class DashboardViewTests(TestCase):
+    
+    # This runs a testing client
+    c=Client()
+    
+    def test_dashboard_view(self):
+        Login.setUp(self)
+        page = self.c.get("/dashboard/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "dashboard.html")
