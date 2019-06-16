a4c0bdc2a53bb853d8a307d4e71bc5608951495f
Garath Davis <garath.davis.gti@gmail.com>
2019-06-16 13:49:21 +0000

    Added Home view tests for logged in and not logged in users

--- home/tests.py
+++ home/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from accounts.client import Login
 
-# Create your tests here.
+
+"""
+Test the Home View for a user being signed in or not signed in
+"""
+class HomeViewTests(TestCase):
+    
+    # This runs a testing client
+    c=Client()
+    
+    # Logged In 
+    def test_home_logged_in_view(self):
+        Login.setUp(self)
+        page = self.c.get("/dashboard/")
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "dashboard.html")
+        
+    
+    # Not Logged In
+    def test_home_not_logged_in_view(self):
+        page = self.client.get("/dashboard/")
+        self.assertEqual(page.status_code, 302)
+        self.assertTemplateNotUsed(page, "dashboard.html")
--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse
 
-# Create your views here.
+"""
+This view is to determine a redirect based on a user being signed in or not signed in
+"""
 def index(request):
     if request.user.is_authenticated():
         return redirect(reverse('dashboard'))
