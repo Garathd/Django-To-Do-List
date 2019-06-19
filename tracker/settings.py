d7eb147380d8e32749f3932eafd7a65acaf1d70d
Garath Davis <garath.davis.gti@gmail.com>
2019-06-19 09:06:31 +0000

    Fixing Heroku Deployment

--- tracker/settings.py
+++ tracker/settings.py
@@ -96,25 +96,25 @@ WSGI_APPLICATION = 'tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
-# if os.environ.get('C9_HOSTNAME') == 'django-project-garath.c9users.io':
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.sqlite3',
-#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#         }
-#     }
-# else:
-#     print("Online DB")
-#     DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
+if hostname:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
+    print("Online DB")
+    DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
